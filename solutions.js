9dfe9303d1274bf840b5962d18890ca04a2b93f7
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-03-18 10:25:29 -0400

    test-functions

--- solutions.js
+++ solutions.js
@@ -2,7 +2,7 @@
 
 /**
  * Write your solutions here.
- *
+ * Added stuff.
  * **Note**: While normally it is good practice to wrap your javascript in an
  * immediately invoked function expression (iife), you should _not_ do that
  * here. If you do, the automated tests will not be able to see your functions.
